bdcae494a0c48119bb2b8e44355595f52448b893
Eric Herman <hermaneric@outlook.com>
2016-11-21 11:12:47 -0500

    another try at stock picker

--- stock_picker_2.rb
+++ stock_picker_2.rb
@@ -1,5 +1,33 @@
-def stock_picker(prices)
-	stock_prices = [0, 0]
+#stock_picker method
+#takes an array of stock prices
+#return a pair of days represent best day to buy and best day to sell
+#buy before you sell
 
+def stock_picker stock_prices
+  best_margin = [0,0,0] # [buy day, sell day, margin amount]
 
+  stock_prices[0..-2].each_with_index do | buy_price, buy_index |
+  	
+    stock_prices[buy_index..-1].each_with_index do | sell_price, sell_index |
+
+      margin = sell_price - buy_price
+      if  margin > best_margin[2] && margin > 0
+        best_margin = [buy_index, buy_index + sell_index, margin]
+      end
+    end
+  end
+  best_margin.take(2)
 end
+
+stock_input = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+
+p stock_picker stock_input
+
+
+
+
+
+
+
+
+
